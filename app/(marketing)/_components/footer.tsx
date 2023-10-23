46eb53dcc5affcb9c542434dee98e4ca0e208818
Pradeepto Sarkar <psrcofficial10@gmail.com>
2023-10-23 16:23:30 +0530

    footer update

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -4,14 +4,22 @@ import { Logo } from "./logo"
 
 export const Footer = () => {
   return (
-    <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
-      <Logo />
-      <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+    <div>
+      <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
+        <Logo />
+        <div className="md:ml-auto w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground">
+          <Button variant="ghost" size="sm">
+            Privacy Policy
+          </Button>
+          <Button variant="ghost" size="sm">
+            Terms & Conditions
+          </Button>
+        </div>
+      </div>
+      <div className="w-full flex items-center justify-center text-muted-foreground">
         <Button variant="ghost" size="sm">
-          Privacy Policy
-        </Button>
-        <Button variant="ghost" size="sm">
-          Terms & Conditions
+          <a href="https://github.com/pradeeptosarkar" target="_blank">
+          Made with ❤️ by Pradeepto Sarkar</a>
         </Button>
       </div>
     </div>
